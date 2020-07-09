d1dc4dbf2bc1859e021629bbc09f580f9db4aad2
BelgianNicky <riatnicky@hotmail.fr>
2020-07-09 15:47:21 +0200

    début home page

---
+++ vue/src/components/layout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">Réparation</router-link>|
+      <router-link to="/about">Config</router-link>|
+      <router-link to="/about">Catalogue</router-link>|
+      <router-link to="/about">Se connecter</router-link>|
+      <router-link to="/about">A propos</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
+    <h1>Bienvenue chez FixScreen</h1>
+
+    <ul>
+      Nos heures d'ouvertures sont :
+      <li>Lundi : 9h-12h | 13h30-17h</li>
+      <li>Mardi : 9h-12h | 13h30-17h</li>
+      <li>Mercredi : 9h-12h | 13h30-15h</li>
+      <li>Jeudi : 9h-12h | 13h30-17h</li>
+      <li>Vendredi : 9h-12h | 13h30-15h</li>
+      <li>Samedi : 9h-13h</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  name: "Home"
 };
 </script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+h1 {
+  margin: 40px 0 0;
+}
+ul {
+  margin: 40px 0 0;
+  list-style-type: none;
+  padding: 0;
+}
+</style>
