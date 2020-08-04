4d1d26b9806706c77014b18603017e0c76df9a40
SaitamEPHEC <m.gassmann@students.ephec.be>
2020-08-04 19:29:17 +0200

    Installation Bootstrap + changement du Header

--- vue/src/components/layout/Header.vue
+++ vue/src/components/layout/Header.vue
@@ -1,12 +1,45 @@
 <template>
   <header class="header">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/reparation">Réparation</router-link>|
-      <router-link to="/config">Config</router-link>|
-      <router-link to="/catalogue">Catalogue</router-link>|
-      <router-link to="/connecter">Se connecter</router-link>|
-      <router-link to="/about">A propos</router-link>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">FixScreen</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#"><router-link to="/" class="routerlink">Home</router-link></b-nav-item>
+            <b-nav-item href="#"><router-link to="/reparation" class="routerlink">Réparation</router-link></b-nav-item>
+            <b-nav-item href="#"><router-link to="/config" class="routerlink">Config</router-link></b-nav-item>
+            <b-nav-item href="#"><router-link to="/catalogue" class="routerlink">Catalogue</router-link></b-nav-item>
+            <b-nav-item href="#"><router-link to="/about" class="routerlink">A propos</router-link></b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
   </header>
 </template>
@@ -18,15 +51,8 @@ export default {
 </script>
 
 <style scoped>
-.header {
-  background: #333;
-  color: #fff;
-  text-align: center;
-  padding: 10px;
-}
-
-.header a {
-  color: #fff;
-  padding-right: 5px;
+.routerlink {
+  display: block;
+  color: rgb(221, 251, 255);
 }
 </style>
