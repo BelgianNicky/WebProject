6c03cc4ff0c2eb7cc3432736aa25b7d6127f749b
SaitamEPHEC <m.gassmann@students.ephec.be>
2020-08-05 18:23:54 +0200

    ajout mdb-vue

---
+++ mdb-vue/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <mdb-carousel
+    :interval="8000"
+    slide
+    :items="items"
+    controlls
+  ></mdb-carousel>
+</template>
+
+<script>
+  import { mdbCarousel } from "mdbvue";
+  export default {
+    name: "CarouselPage",
+    components: {
+      mdbCarousel
+    },
+    data() {
+      return {
+        items: [
+          {
+            img: true,
+            src:
+              "https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg"
+          },
+          {
+            img: true,
+            src:
+              "https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg"
+          },
+          {
+            img: true,
+            src:
+              "https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg"
+          }
+        ]
+      };
+    }
+  };
+</script>
