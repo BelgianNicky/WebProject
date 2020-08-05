f5663c744965879573a93d04f91ff195f79d2e07
BelgianNicky <riatnicky@hotmail.fr>
2020-08-06 00:29:03 +0200

    register form with checks

--- mdb-vue/src/views/Inscription.vue
+++ mdb-vue/src/views/Inscription.vue
@@ -1,5 +1,89 @@
 <template>
-  <div class="inscription">
-    <h1>Page s'inscrire</h1>
+  <div class="w-50 mx-auto">
+    <mdb-container>
+      <form id="register" @submit="checkForm" action="debug" novalidate="true">
+        <!-- method="post" --->
+        <br />
+
+        <p class="h4 text-center mb-4">Inscription</p>
+            <p v-if="errors.length">
+              <b>Problème lors de l'inscription:</b>
+                <ul>
+                  <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+              </ul>
+          </p>
+        <label for="name" class="grey-text">Votre nom</label>
+        <input type="text" id="name" class="form-control" v-model="name" name="name" />
+        <br />
+        <label for="email" class="grey-text">Votre email</label>
+        <input type="email" id="email" class="form-control" v-model="email" name="email" />
+        <br />
+        <label for="pwd" class="grey-text">Votre mot de passe</label>
+        <input type="password" id="pwd" class="form-control" v-model="pwd" name="pwd" />
+        <br />
+        <label for="confirmPwd" class="grey-text">Confirmez votre mot de passe</label>
+        <input
+          type="password"
+          id="confirmPwd"
+          class="form-control"
+          v-model="confirmPwd"
+          name="confirmPwd"
+        />
+        <div class="text-center mt-4">
+          <button class="btn btn-unique" type="submit">S'inscrire !</button>
+        </div>
+      </form>
+    </mdb-container>
   </div>
 </template>
+
+<script>
+export default {
+  data: function(){ return {
+    errors: [],
+    name: null,
+    age: null,
+    movie: null
+    };
+  },
+  methods: {
+    checkForm (e) {
+      if (this.name && this.email && this.pwd === this.confirmPwd) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push("Nom obligatoire");
+      }
+      if (!this.email) {
+        this.errors.push('Email obligatoire');
+      } 
+      else if (!this.validEmail(this.email)) {
+        this.errors.push('Email non valide');
+      }
+      if (this.pwd !== this.confirmPwd) {
+        this.errors.push("Mots de passes différents");
+      }
+      e.preventDefault();
+    },
+        validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  font-size: xx-large;
+}
+
+li {
+  color: red;
+}
+</style>
