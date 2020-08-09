30b79e82b3f4a61f1379604d1ef4d7bbd5feb94c
max-lambert <33155378+max-lambert@users.noreply.github.com>
2020-08-09 18:29:37 +0200

    Add files via upload

---
+++ BDD/server.js
@@ -0,0 +1,30 @@
+function dbconnect (){
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'maximilien',
+    password : 'root',
+    database : 'mybase'
+  });
+
+  connection.connect();
+
+  return connection
+}
+
+function Insertuser(){
+  var db=dbconnect();
+
+  var username= "nicky"
+  var password= "azerty123"
+  var email= "nick@yopmail.com"
+  var full_name= "nicky riat"
+  var id_habitation= "1"
+  var id_panier="2"
+
+  let crypto = require('crypto');
+  const hash = crypto.createHmac('sha256',password)
+                .update('whateverrr')
+                .digest('hex');
+  db.query('INSERT INTO user SET username=?, password=?, email=?, full_name=?, id_habitation=?, id_panier=?')
+}
