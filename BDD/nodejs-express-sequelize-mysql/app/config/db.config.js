c196b9a5f085f571faa9b715a1a2098eeec46548
max-lambert <HE201481@students.ephec.be>
2020-08-12 02:26:19 +0200

    connection db

---
+++ BDD/nodejs-express-sequelize-mysql/app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: "51.210.149.38",
+  PORT: "3306",
+  USER: "maximilien",
+  PASSWORD: "lambert",
+  DB: "mybase",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
