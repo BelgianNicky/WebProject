9edec7b740f7e5555fadc0c2e501f0b61dad768d
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-08-16 02:04:36 +0200

    Amélioration et fin de la fonction createUser (tout les cas sont normalement pris en compte)

--- BDD/nodejs-express-sequelize-mysql/app/controllers/users.controller.js
+++ BDD/nodejs-express-sequelize-mysql/app/controllers/users.controller.js
@@ -9,27 +9,25 @@ const Habitation = db.habitation;
 // si l'username et l'email ne sont pas déjà présents dans la DB.
 exports.createUsers = (req, res) => {
 
-// Check si le username existe déjà dans la base de données
-Users.findAll({where: { username : req.body.username }})
-  .then(usernameData =>{
-    if(usernameData != null  && usernameData.length > 0){
-      res.send('username already exists')
-    }
-    else{
-      // Check si l'email existe déjà dans la base de données
-      Users.findAll({where: {email : req.body.email }})
-      .then(emailData =>{
-        if(emailData != null  && emailData.length > 0){
-          res.send('email already exists')
-        }
-        else{
-          // Check si la ville existe déjà dans la base de données
-          Habitation.count({ where: { ville : req.body.ville } })
-          .then(count => {
-            // Si la ville existe déjà dans la base de données
-            if(count > 0) {
-              Habitation.findOne({ where: { ville : req.body.ville }})
-              .then(habitationData => {
+  // Check si le username existe déjà dans la base de données
+  Users.findAll({where: { username : req.body.username }})
+    .then(usernameData =>{
+      if(usernameData != null  && usernameData.length > 0){
+        res.send('username already exists')
+      }
+      else{
+        // Check si l'email existe déjà dans la base de données
+        Users.findAll({where: {email : req.body.email }})
+        .then(emailData =>{
+          if(emailData != null  && emailData.length > 0){
+            res.send('email already exists')
+          }
+          else{
+            // Check si la ville existe déjà dans la base de données
+            Habitation.findAll({ where: { ville : req.body.ville }})
+            .then(habitationData => {
+              // Si elle existe, créer un user avec l'ID de l'habitation correspondante
+              if(habitationData != null  && habitationData.length > 0){
                 // Création d'un panier vide, à attribuer au user
                 ControllerPanier.createPanier().then(panier => {
                   const {dataValues} = panier;
@@ -41,7 +39,7 @@ Users.findAll({where: { username : req.body.username }})
                     full_name: req.body.full_name,
                     adresse: req.body.adresse,
                     panierId: dataValues.id,
-                    habitationId: habitationData.id
+                    habitationId: habitationData[0].id
                   };
                   // Création du user
                   Users.create(users)
@@ -54,80 +52,73 @@ Users.findAll({where: { username : req.body.username }})
                         err.message || "Some error occurred while creating the User with 'habitation' already existing in the database."
                     });
                   });
-                })
-              })
-              .catch(err => {
-                res.status(500).send({
-                  message:
-                    err.message || "Some error occurred while creating the User with 'habitation' already existing in the database."
-                });
-              });
-            }
-            // Si la ville n'existe pas dans la base de données, on vient la créer
-            else{
-              const habitation = {
-                ville: req.body.ville,
-                code_postal: req.body.code_postal,
-              };
-              Habitation.create(habitation)
-              .then(habitationData=> {
-                // Création d'un panier vide, à attribuer au user
-                ControllerPanier.createPanier().then(panier => {
-                  const {dataValues} = panier;
-        
-                  const users = {
-                    username: req.body.username,
-                    password: req.body.password,
-                    email: req.body.email,
-                    full_name: req.body.full_name,
-                    adresse: req.body.adresse,
-                    panierId: dataValues.id,
-                    habitationId: habitationData.id
-                  };
-                  // Création du user
-                  Users.create(users)
-                  .then(data => {
-                    res.send(data);
-                  })
-                  .catch(err => {
-                    res.status(500).send({
-                      message:
-                        err.message || "Some error occurred while creating the User with 'habitation' already existing in the database."
+                })                
+              }
+              // Si elle n'existe pas, créer une nouvelle ville et attribue l'ID de cette nouvelle ville au user
+              else{
+                const habitation = {
+                  ville: req.body.ville,
+                  code_postal: req.body.code_postal,
+                };
+                Habitation.create(habitation)
+                .then(habitationCreatedData=> {
+                  // Création d'un panier vide, à attribuer au user
+                  ControllerPanier.createPanier().then(panier => {
+                    const {dataValues} = panier;
+          
+                    const users = {
+                      username: req.body.username,
+                      password: req.body.password,
+                      email: req.body.email,
+                      full_name: req.body.full_name,
+                      adresse: req.body.adresse,
+                      panierId: dataValues.id,
+                      habitationId: habitationCreatedData.id
+                    };
+                    // Création du user
+                    Users.create(users)
+                    .then(data => {
+                      res.send(data);
+                    })
+                    .catch(err => {
+                      res.status(500).send({
+                        message:
+                          err.message || "Some error occurred while creating the User with 'habitation' already existing in the database."
+                      });
                     });
-                  });
+                  })
                 })
-              })
-              .catch(err => {
-                res.status(500).send({
-                  message:
-                    err.message || "Some error occurred while creating habitation"
+                .catch(err => {
+                  res.status(500).send({
+                    message:
+                      err.message || "Some error occurred while creating habitation"
+                  });
                 });
+              } 
+            })
+            .catch(err => {
+              res.status(500).send({
+                message:
+                  err.message || "Some error occurred while creating the User with 'habitation' already existing in the database."
               });
-            }
-          })
-          .catch(err => {
-            res.status(500).send({
-              message:
-                err.message || "Some error occurred while counting the number of 'habitation' found with a certain 'ville' name"
             });
+          }
+        })
+        .catch(err => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while finding User with 'email'"
           });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while finding User with 'email'"
         });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while finding User with 'username'"
       });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message:
-        err.message || "Some error occurred while finding User with 'username'"
     });
-  });
- };
+}
 
  //retourne un user en fonction de son id
  exports.findOneUsers= (req, res) => {
