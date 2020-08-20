56ba616d36a3a7729f4f4accce2032260023a269
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-08-21 01:53:25 +0200

    Update produit.routes.js

--- BDD/nodejs-express-sequelize-mysql/app/routes/produit.routes.js
+++ BDD/nodejs-express-sequelize-mysql/app/routes/produit.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
     var express = require('express');
     var router = express.Router();
 
-    var ProduitController = require('../controllers/Produit.controller');
+    var ProduitController = require('../controllers/produit.controller');
 
     router.get('/:id', ProduitController.findOneProduit);
     router.post('/', ProduitController.createProduit);
