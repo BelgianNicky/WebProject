6499f8cd031e84320f07824de77b48611ad0bb59
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-08-21 01:07:41 +0200

    Update db.config.js

--- BDD/nodejs-express-sequelize-mysql/app/config/db.config.js
+++ BDD/nodejs-express-sequelize-mysql/app/config/db.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   HOST: "51.210.149.38",
   PORT: '3306',
+  //USER: "mathias",
   USER: "maximilien",
+  //PASSWORD: "gassmann",
   PASSWORD: "lambert",
   DB: "mybase",
   dialect: "mysql",
