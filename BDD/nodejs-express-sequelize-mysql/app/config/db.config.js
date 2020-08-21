35bd90a5c4fbc0dd0a3a36f978721a29919f0402
BelgianNicky <riatnicky@hotmail.fr>
2020-08-21 14:26:19 +0200

    regulation noms api

--- BDD/nodejs-express-sequelize-mysql/app/config/db.config.js
+++ BDD/nodejs-express-sequelize-mysql/app/config/db.config.js
@@ -1,16 +1,18 @@
 module.exports = {
   HOST: "51.210.149.38",
-  PORT: '3306',
+  PORT: "3306",
   //USER: "mathias",
-  USER: "maximilien",
+  //USER: "maximilien",
+  USER: "nicky",
   //PASSWORD: "gassmann",
-  PASSWORD: "lambert",
+  //PASSWORD: "lambert",
+  PASSWORD: "riat",
   DB: "mybase",
   dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
--- BDD/nodejs-express-sequelize-mysql/app/routes/produit.routes.js
+++ BDD/nodejs-express-sequelize-mysql/app/routes/produit.routes.js
@@ -1,14 +1,14 @@
-module.exports = app => {
-    var express = require('express');
-    var router = express.Router();
+module.exports = (app) => {
+  var express = require("express");
+  var router = express.Router();
 
-    var ProduitController = require('../controllers/produit.controller');
+  var ProduitController = require("../controllers/produit.controller");
 
-    router.get('/:id', ProduitController.findOneProduit);
-    router.post('/', ProduitController.createProduit);
-    router.put('/:id', ProduitController.updateProduit);
-    router.delete('/:id', ProduitController.deleteProduit);
-    router.get('/', ProduitController.findAllProduit);
+  router.get("/:id", ProduitController.findOneProduit);
+  router.post("/", ProduitController.createProduit);
+  router.put("/:id", ProduitController.updateProduit);
+  router.delete("/:id", ProduitController.deleteProduit);
+  router.get("/", ProduitController.findAllProduit);
 
-    app.use('/api/Produit',router);
+  app.use("/api/produit", router);
 };
