9a2fbe50b6de672d4b72df5917cab411211b57aa
BelgianNicky <riatnicky@hotmail.fr>
2020-08-23 22:56:03 +0200

    update a propos

--- mdb-vue/src/components/layout/Apropos.vue
+++ mdb-vue/src/components/layout/Apropos.vue
@@ -7,16 +7,14 @@
         img-alt="Mathias"
         img-top
       >
-        <b-card-text>Étudiant à l'EPHEC en Technologie de l'informatique. Gérant de <b>Logistick</b> et développeur web.</b-card-text>
+        <b-card-text>
+          Étudiant à l'EPHEC en Technologie de l'informatique. Gérant de
+          <b>Logistick</b> et développeur web.
+        </b-card-text>
       </b-card>
 
-      <b-card
-        title="Nicky Riat"
-        :img-src="require('@/assets/bon_toutou.jpg')"
-        img-alt="Nicky"
-        img-top
-      >
-        <b-card-text>Doux</b-card-text>
+      <b-card title="Nicky Riat" :img-src="require('@/assets/nickyR.jpg')" img-alt="Nicky" img-top>
+        <b-card-text>Développeur web pour Logistick, principalement front end</b-card-text>
       </b-card>
 
       <b-card
