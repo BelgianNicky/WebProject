a424b389748a574b7159dba7d85667b4d2a0803c
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-08-26 16:54:57 +0200

    update server.js pour que le serveur api accepte notre site en https

--- BDD/nodejs-express-sequelize-mysql/server.js
+++ BDD/nodejs-express-sequelize-mysql/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 var corsOptions = {
   //origin: "http://localhost:8081",
-  origin: "http://logistick.be"
+  origin: "https://logistick.be"
 };
 
 app.use(cors(corsOptions));
