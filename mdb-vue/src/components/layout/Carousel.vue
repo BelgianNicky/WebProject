0651a54b79bf7f0a3bd1761b4af63e8a1b2743a6
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-08-27 13:26:02 +0200

    Update Carousel.vue

--- mdb-vue/src/components/layout/Carousel.vue
+++ mdb-vue/src/components/layout/Carousel.vue
@@ -1,39 +1,37 @@
 <template>
-    <div>
-        <b-carousel
-            id="carousel-1"
-            v-model="slide"
-            :interval="4000"
-            controls
-            indicators
-            background="#ababab"
-            img-width="1024"
-            img-height="480"
-            style="text-shadow: 1px 1px 2px #333;"
-            @sliding-start="onSlideStart"
-            @sliding-end="onSlideEnd"
-        >
-            <!-- Text slides with image -->
-            <b-carousel-slide
-            caption="Équipements Gaming dernier cri !"
-            text="Profitez de nos offres exclusives, jusqu'à -50%"
-            :img-src="require('@/assets/Carousel_5.jpg')"
-            ></b-carousel-slide>
+    <b-carousel
+        id="carousel-1"
+        v-model="slide"
+        :interval="4000"
+        controls
+        indicators
+        background="#ababab"
+        img-width="1024"
+        img-height="480"
+        style="text-shadow: 1px 1px 2px #333;"
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"
+    >
+        <!-- Text slides with image -->
+        <b-carousel-slide
+        caption="Équipements Gaming dernier cri !"
+        text="Profitez de nos offres exclusives, jusqu'à -50%"
+        :img-src="require('@/assets/Carousel_5.jpg')"
+        ></b-carousel-slide>
 
-            <b-carousel-slide
-            caption=""
-            text=""
-            :img-src="require('@/assets/Carousel_7.jpg')"
-            ></b-carousel-slide>
+        <b-carousel-slide
+        caption=""
+        text=""
+        :img-src="require('@/assets/Carousel_7.jpg')"
+        ></b-carousel-slide>
 
-            <b-carousel-slide
-            caption="Offre Chaises gamings"
-            text="2 chaises gaming achetées, 1 offerte"
-            :img-src="require('@/assets/Carousel_6.jpg')"
-            ></b-carousel-slide>
-            
-        </b-carousel>
-    </div>
+        <b-carousel-slide
+        caption="Offre Chaises gamings"
+        text="2 chaises gaming achetées, 1 offerte"
+        :img-src="require('@/assets/Carousel_6.jpg')"
+        ></b-carousel-slide>
+        
+    </b-carousel>
 </template>
 
 <script>
